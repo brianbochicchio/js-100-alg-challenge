d4a62b529c223f17feb850b3dea08615dc5ad90d
Brian Bochicchio <bbochicchio@gmail.com>
2022-01-06 06:37:05 -0600

    Create absoluteValuesSumMinization.ts
    First draft.  See comments for concerns about the solution

---
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -0,0 +1,39 @@
+/* In the video provided by the challenge editor we are seeking the median value of the "pre-sorted" arrays below
+    I'm not sure that is really what the challenge is asking... 
+    https://github.com/PacktPublishing/100-JavaScript-Algorithm-Challenges/tree/master/absoluteValuesSumMinization
+
+    Per my reading from a few differnt sources: If an array is sorted, median is the middle element of an array
+     in case of odd number of elements in an array and when number of elements in an array is even than 
+     it will be an average of two middle elements. If the array is not sorted first task is to sort the 
+     array and then only the given logic can be applied    
+  
+    In the authors solution, he doesn't sort the array or seek an average of the two middle values.  He simply
+    divides the lenth of the array by 2.  In the case of even he subtracts 1 and in the case of of odd he 
+    uses the floor.  The resulting answers are 4,4,7,7
+
+    My results using the logic above are 4,6,6,6 
+
+    I need to re-visit this 
+*/
+
+function absoluteValuesSumMinimization(a: number[]): number {
+    a.sort()
+    if (a.length % 2 === 0) {
+        let middle = Math.floor(a.length / 2)
+        console.log(middle)
+        let median = Math.ceil((a[middle] + a[middle]) / 2)
+        console.log(median)
+        return median
+        
+    } else {
+        return a[Math.floor(a.length / 2)];
+    }
+
+   
+
+}
+
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
