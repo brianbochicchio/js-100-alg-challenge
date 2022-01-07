f2518e8c33313d2256cbec19afc7fb5accfee10a
Brian Bochicchio <bbochicchio@gmail.com>
2022-01-07 16:39:08 -0600

    Refined addBorder
    Started modifying addBorder so it will apply appropriate padding to shorter strings.  The goal is to retain the "shape" of the border.

--- addBorderToText/addBorder.ts
+++ addBorderToText/addBorder.ts
@@ -4,18 +4,29 @@ Calling addBorder would result in the following array in the console ["*****", "
 
 Two ways to make this better would be to base the border mark on the longest array element and 
 have it pad shorter ones with spaces so it would always render as a square or rectangle. 
-   
-
 
 */
 
 function addBorder(picture :string[]) : any {
-    const maxBorderLen = picture.length + 2;
-    const borderMark = "*"
-    const longBorder = borderMark.repeat(maxBorderLen);
+    const borderMark : string = "*"
+    let maxBorderLen :number = 0;
+    for (let i = 0; i < picture.length; i++) {
+        if (picture[i].length > maxBorderLen) {
+            maxBorderLen = picture[i].length;
+        }  
+        
+    }
+    maxBorderLen += 2;    
+    const longBorder : string = borderMark.repeat(maxBorderLen);
 
     for (let i = 0; i < picture.length; i++) {
-        picture[i] = `${borderMark}${picture[i]}${borderMark}`;  
+        let padding = maxBorderLen - picture[i].length;
+        picture[i] = picture[i].padStart(padding, " ");
+        picture[i] = picture[i].padEnd(padding +2, " ");
+
+        // now have a thread to tug on, need to work on padding formula some more
+
+        picture[i] = `${borderMark}${picture[i]}${borderMark}`;
     }
     picture.push(longBorder);
     picture.unshift(longBorder);
@@ -23,6 +34,6 @@ function addBorder(picture :string[]) : any {
 
     return picture;
 }
-console.log(addBorder(["abc", "ded"]));
-console.log(addBorder(["abc"]));
-console.log(addBorder(["abc", "def", "ghi", "nachos"]));
+//console.log(addBorder(["abc", "ded"]));
+//console.log(addBorder(["abc"]));
+console.log(addBorder(["abc", "def", "ghi", "nachos", 'z']));
