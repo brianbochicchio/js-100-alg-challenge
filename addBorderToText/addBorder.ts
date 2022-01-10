9e6292e0dcc2aa2b5d23c7832dabdf010b1265ce
Brian Bochicchio <bbochicchio@gmail.com>
2022-01-10 16:56:33 -0600

    Update addBorder.ts
    Worked out to the  logic to get retrain the "square" shape.

--- addBorderToText/addBorder.ts
+++ addBorderToText/addBorder.ts
@@ -5,6 +5,10 @@ Calling addBorder would result in the following array in the console ["*****", "
 Two ways to make this better would be to base the border mark on the longest array element and 
 have it pad shorter ones with spaces so it would always render as a square or rectangle. 
 
+Got the logic in for the comment above. Need to re-factor the code. Code associated with maxBorderLen is a 
+little klugdy because in one case I am adding 2 to it only to take those two away. I think it could be better 
+organized too.  
+
 */
 
 function addBorder(picture :string[]) : any {
@@ -20,12 +24,14 @@ function addBorder(picture :string[]) : any {
     const longBorder : string = borderMark.repeat(maxBorderLen);
 
     for (let i = 0; i < picture.length; i++) {
-        let padding = maxBorderLen - picture[i].length;
-        picture[i] = picture[i].padStart(padding, " ");
-        picture[i] = picture[i].padEnd(padding +2, " ");
-
-        // now have a thread to tug on, need to work on padding formula some more
-
+        let whiteSpace = ( maxBorderLen - 2) - picture[i].length;
+        console.log(whiteSpace)
+        let leftPad = Math.floor(whiteSpace / 2);
+        let rightPad = Math.ceil(whiteSpace / 2);
+        console.log(leftPad,rightPad);
+        picture[i] = picture[i].padStart(picture[i].length + leftPad, " ");
+        picture[i] = picture[i].padEnd(picture[i].length + rightPad, " ");
+      
         picture[i] = `${borderMark}${picture[i]}${borderMark}`;
     }
     picture.push(longBorder);
@@ -36,4 +42,4 @@ function addBorder(picture :string[]) : any {
 }
 //console.log(addBorder(["abc", "ded"]));
 //console.log(addBorder(["abc"]));
-console.log(addBorder(["abc", "def", "ghi", "nachos", 'z']));
+console.log(addBorder(["ghi", "nachos", 'z', 'says', "Cracker Barrel"]));
