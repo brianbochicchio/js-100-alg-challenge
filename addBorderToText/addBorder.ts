6a7206f722717b53ac6275c811ae8b518690ccf1
Brian Bochicchio <bbochicchio@gmail.com>
2022-01-11 08:36:04 -0600

    Update addBorder.ts
    Fixed comment and changed picture to wordsToBorder so the name makes more sense.

--- addBorderToText/addBorder.ts
+++ addBorderToText/addBorder.ts
@@ -14,48 +14,48 @@ Expanding on the Challenge:
   '*  spoon   *',
   '************' ]
 
-Have the function accept strings of various lengths and center align the output with some extra padding for a clean
+Have the function accept strings of various lengths and center align the output with some extra padding for a clean border frame.
 
 */
 
 const borderMark : string = '*';
 
 // Need this to calculate the top and bottom border lengths and padding amount
-const getLongestStringLength = (picture : string[]) : number => {
+const getLongestStringLength = (wordsToBorder : string[]) : number => {
     let longestStringLength = 0;
-    for (let i = 0; i < picture.length; i++) {
-        if (picture[i].length > longestStringLength) {
-            longestStringLength = picture[i].length;
+    for (let i = 0; i < wordsToBorder.length; i++) {
+        if (wordsToBorder[i].length > longestStringLength) {
+            longestStringLength = wordsToBorder[i].length;
         }  
     }   
     return longestStringLength;
 }
 
-const addTopBottomBorders = (picture : string[], longestStringLength: number) : any =>  {
+const addTopBottomBorders = (wordsToBorder : string[], longestStringLength: number) : any =>  {
     longestStringLength += 4;    
     const border : string = borderMark.repeat(longestStringLength);
-    picture.push(border);
-    picture.unshift(border);
+    wordsToBorder.push(border);
+    wordsToBorder.unshift(border);
 }
 
-function addBorder(picture :string[]) : any {
-    let longestStringLength = getLongestStringLength(picture);
+function addBorder(wordsToBorder :string[]) : any {
+    let longestStringLength = getLongestStringLength(wordsToBorder);
 
-    for (let i = 0; i < picture.length; i++) {
+    for (let i = 0; i < wordsToBorder.length; i++) {
         
-        let totalPadding : number = longestStringLength - picture[i].length;
+        let totalPadding : number = longestStringLength - wordsToBorder[i].length;
         let leftPad : number = Math.floor(totalPadding / 2);
         let rightPad : number = Math.ceil(totalPadding / 2);
     
-        picture[i] = picture[i].padStart(picture[i].length + leftPad + 1, " "); 
+        wordsToBorder[i] = wordsToBorder[i].padStart(wordsToBorder[i].length + leftPad + 1, " "); 
         // adding padding changed the current item's length
-        picture[i] = picture[i].padEnd(picture[i].length + rightPad + 1, " ");  
-        picture[i] = `${borderMark}${picture[i]}${borderMark}`;
+        wordsToBorder[i] = wordsToBorder[i].padEnd(wordsToBorder[i].length + rightPad + 1, " ");  
+        wordsToBorder[i] = `${borderMark}${wordsToBorder[i]}${borderMark}`;
     }
 
-    addTopBottomBorders(picture, longestStringLength);
+    addTopBottomBorders(wordsToBorder, longestStringLength);
 
-    return picture;
+    return wordsToBorder;
 }
 //console.log(addBorder(["123", "456"]));
 //console.log(addBorder(["w", "x", "y", "z"]));
